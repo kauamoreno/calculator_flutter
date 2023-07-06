a347cdd7d71fe874499ef787211b9700193f8a62
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-07-05 21:57:50 -0300

    feat: Finalizando configs da tela e gancho para model

--- lib/screens/Calculator.dart
+++ lib/screens/Calculator.dart
@@ -1,17 +1,41 @@
+import 'package:calculadora_flutter/models/Memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/Display.dart';
 import '../components/Keyboard.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
+
   const Calculator({super.key});
 
   @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+  final Memory memory = Memory();
+
+  _onPressed(String comando){
+    setState(() {
+      memory.aplicarComando(comando);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    //Para sempre ficar em vertical o app, nao permitir rolagem
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Column(
         children: [
-          Display(texto: '123.3'),
-          Keyboard()
+          Display(texto: memory.value),
+          Keyboard(cb: _onPressed )
         ],
       ),
     );
